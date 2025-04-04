8d1ada8ff282e51e3b434ab3fc6cfceeafa68420
Halin Cyril Yvon Jacques (BEG/EOR2) <CyrilYvonJacques.Halin@de.bosch.com>
2025-04-04 16:50:41 +0200

    feat: ruff linter

--- random_fact.py
+++ random_fact.py
@@ -13,7 +13,7 @@ def get_random_fact():
         response = requests.get(url)
         response.raise_for_status()  # Raise an error for bad HTTP responses
         fact_data = response.json()
-        return fact_data['text']
+        return fact_data["text"]
     except requests.exceptions.RequestException as e:
         return f"Failed to fetch a fact: {e}"
 
